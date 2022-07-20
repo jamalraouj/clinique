ac493b033d0f0f771aa55334ef3ba40cf691c153
chaimae mita <chaimae.mita@gmail.com>
2022-07-20 13:17:12 +0100

    I deleted some migrations

---
+++ src/Form/MenuType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Menu;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('genre_repas')
+            ->add('nom_repas')
+            ->add('aperitifs')
+            ->add('desert')
+            ->add('categorie')
+            ->add('jour')
+            ->add('temps')
+            ->add('repas_alternative')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Menu::class,
+        ]);
+    }
+}
