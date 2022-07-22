e06640bfb19426b91242b4a1f5c226380370f0b2
jamalraouj <jamalrwj@gmail.com>
2022-07-22 16:15:39 +0100

    I add template use and form user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User 
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
