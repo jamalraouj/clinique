f2189a2ccdd967eab8c66cb89c7b21d914aa9ce4
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-08-14 18:09:56 +0100

    Added Code to handle Uploaded Images

--- src/Controller/MedecinController.php
+++ src/Controller/MedecinController.php
@@ -45,9 +45,70 @@ class MedecinController extends AbstractController
         $plaintextPassword = ''; // get the plain password from the form
 
         if ($formMedecin->isSubmitted() && $formMedecin->isValid()) {
+                echo "<pre>";
+                var_dump($user); echo "</pre>";
+                echo "#########-------###############" ;
+                echo "<pre>";
+                var_dump($medecin); echo "</pre>"; exit ;
+                // if this condition is not true than it means a file has uploaded , not an empty field file input .
+                  if(!($_FILES['medecin']['error']['image_medecin'] == UPLOAD_ERR_NO_FILE)) {
+                    // This file superglobal gets all the information from the file that we want to upload using an input from a form
+                    $photo = $_FILES['medecin'];
+                    // echo "<pre>" ;
+                    // print_r($photo);
+                    // echo "</pre>"; exit ;
+                    // $_files array contains  : name/ type / tmp_name / error / size
+                    $fileName = $photo['name']['image_medecin'];
+                    $fileTmpName = $photo['tmp_name']['image_medecin'];
+                    $fileSize = $photo['size']['image_medecin'];
+                    $fileError = $photo['error']['image_medecin'];
+                
+                    // to get the extension of the file
+                    $fileExt = explode('.', $fileName);
+                    // Make sure that always the extension comes in small letters
+                    $fileActualExt = strtolower(end($fileExt));
+                
+                    // inside this array we gonna tell it wich type of files we want to allow inside the website
+                    $allowed = array('jpg' , 'jpeg' , 'png');
+                
+                    if(in_array($fileActualExt , $allowed)) {
+                        // if the file error is equal to 0 that means that we had no erros uploading this file 
+                        if($fileError == 0){
+                
+                            if($fileSize < 5000000){
+                                /* Before we upload the file we have to make sure that when we do upload the file it gets
+                                a proper name because for example a file called test.JPEG to uploads folder and someone 
+                                else later on uploads a image that has the exact same name test.JPEG it will actually 
+                                overwrite the existing image inside the uploads folder meaning that the other user who
+                                upload an image will get his image deleted so in order to prevent that we're going to 
+                                create a unique id wich gets inserted and replaced with the actual name of the file when
+                                it was uploaded so instead of it being named test.JPEG coul actually get named something like 
+                                bunch of numbers .JPEG */
+                                $fileNameNew = "doctor".uniqid().".". $fileActualExt;
+                                $fileDestination = $medecin-> UPLOAD_FOLDER  . "/medecin/" . $fileNameNew ;
+                                move_uploaded_file($fileTmpName,$fileDestination);
+                                $medecin->setImageMedecin($fileNameNew);
+                            } else {
+                                echo "Your file is too big !";
+                                exit ;
+                            }
+                
+                            } else {
+                            echo "There was an error uploading your file !";
+                            exit ;
+                            }
+                
+                    } else {
+                        echo "You can not upload files of this type !";
+                        exit ;
+                    }
+                }
+            // Making Sure that the matricule is UpperCase
+            strtoupper($medecin->getMatricule());    
+            // Adding the medecin to the database
             $medecinRepository->add($medecin, true);
 
-            $user->setFkMedecin($medecin->getId());
+            $user->setFkMedecin($medecin);
             $user->setUserRole('ROLE_MEDECIN');
             $hashedPassword = $passwordHasher->hashPassword(
                 $user,
