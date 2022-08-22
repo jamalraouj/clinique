16ef637ec26619f19c8658708ed4ef9467c9acc2
jamalraouj <jamalrwj@gmail.com>
2022-08-22 13:16:48 +0100

    I show some Infos of doctors in dossier patient

---
+++ src/Form/ChambreType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Chambre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChambreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nombre')
+            ->add('status')
+            ->add('type')
+            ->add('Description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Chambre::class,
+        ]);
+    }
+}
